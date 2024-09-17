6c7e8fcd6a66ada5ceaddd6583621cacb5eca13d
Altony Foote <evv2dx@virginia.edu>
2024-09-16 21:44:33 -0400

    commit prop logic main file

--- Cs2120f24/Lectures/02_prop_logic/formal/Main.lean
+++ Cs2120f24/Lectures/02_prop_logic/formal/Main.lean
@@ -1,4 +1,7 @@
-import «Cs2120f24».Lectures.«02_prop_logic».formal.properties
+import Cs2120f24.Lectures.«02_prop_logic».formal.interpretation
+import Cs2120f24.Lectures.«02_prop_logic».formal.properties
+import Cs2120f24.Lectures.«02_prop_logic».formal.utilities
+
 namespace cs2120f24
 
 open PLExpr
@@ -6,60 +9,48 @@ open PLExpr
 
 /-!
 SYNTAX
-
-Suppose I want to write some propositional logic expressions
-using the variable expressions, P, Q, and R, and building up
-larger propositions using the propositional logic expression-
-constructing operators. Here's how you do it.
 -/
 
--- First define a disting variable for each variable expression
-def v₀ : BoolVar := BoolVar.mk 0    -- abstract syntax
-def v₁ : BoolVar := ⟨1⟩             -- Lean notation for mk
+-- Variables
+def v₀ : BoolVar := BoolVar.mk 0
+def v₁ : BoolVar := ⟨1⟩   -- Lean for structure constructor (mk)
 def v₂ : BoolVar := ⟨2⟩
 
 /-!
-Now you define the variable expressions you want to use. The
-first line is using abstract syntax. The next two use our own
-(non-standard) notation, desugaring to exactly that abstract
-syntax.
+Variable expressions
 -/
 def P : PLExpr := PLExpr.var_expr v₀
 def Q : PLExpr := { v₁ }  -- our notation for var_expr constructor
 def R : PLExpr := { v₂ }
 
 /-
-Now that you have three variable expressions to work with,
-you can use logical expression "connectives" (operators) to
-build bigger expressions. Consider the expression, P ∧ Q,
-for example. First we'll write it using abstract syntax,
-then using concrete syntax.
+Operator expression: abstract syntax
 -/
 
--- abstract syntax
 def P_and_Q_abstract : PLExpr :=
   (PLExpr.bin_op_expr BinOp.and P Q)
 
--- Concrete syntax (standard math book) notation
+-- Exact same expression using standard notation
 def P_and_Q_concrete := P ∧ Q
 
--- Notation de-sugars to that astract syntax
+-- de-sugars to the underlying astract syntax
 #reduce P_and_Q_concrete
 
 
 /-!
 INTERPRETATIONS
+
+The apparent ordering is off, backwards from
+what we'd expect. For now it doesn't matter.
+We've got it on our list of things to fix.
+This example counts the number of variables in
+(P ∧ Q), it's 2, and a list of all 2^2 = 4
+possible interpretations for that expression
+are returned.
 -/
-
-/-
-Here's how we can see a list of interpretations
-for a given expressions.
--/
-#reduce interpStringsFromInterps
-          (listInterpsFromExpr (P ∧ Q ∨ R))
-          3 -- number of variables here
-
-
+#reduce listListStringFromListInterps
+          (listInterpsFromExpr (P ∧ Q))
+          2
 /-!
 It's often helpful to list arguments
 to functions properly indented across
@@ -76,8 +67,8 @@ starting with all true and descending to all
 false. (We have a note to clean this up.)
 -/
 
-#eval! (truthTableOutputs (P))
-#eval! (truthTableOutputs (P ∨ Q))
+#eval! (truthTableOutputVector (P))
+#eval! (truthTableOutputVector (P ∧ Q))
 
 
 /-!
@@ -148,7 +139,7 @@ def p0 : PLExpr := itsRaining ∧ sprinklerOn
 /-!
 The sprinler's on and it's raining.
 -/
-def p1  : PLExpr := sorry
+def p1  : PLExpr := sprinklerOn ∧ itsRaining
 
 /-!
 If it's raining, then if the sprinkler's on, then it's
@@ -157,7 +148,7 @@ then that) expressions in natural language are written
 formally in propositional and predicate logic using the
 implication (implies) operator, imp (⇒ in our notation).
 -/
-def p2  : PLExpr := itsRaining ⇒ sprinklerOn ⇒ p0
+def p2  : PLExpr := itsRaining ⇒ (sprinklerOn ⇒ p0)
 
 /-!
 If it's raining and the sprinkler's running, then it's raining.
@@ -167,22 +158,22 @@ def p3  : PLExpr := (itsRaining ∧ sprinklerOn) ⇒ itsRaining
 /-!
 If it's raining ,then it's raining or the sprinkler's running.
 -/
-def p4  : PLExpr := sorry
+def p4  : PLExpr := itsRaining ⇒ (itsRaining ∨ sprinklerOn)
 
 /-!
 If the sprinkler's running, it's raining or the sprinkler's running.
 -/
-def p5  : PLExpr := sorry
+def p5  : PLExpr := sprinklerOn ⇒ (itsRaining ∨ sprinklerOn)
 
 /-!
 Whenever it's raining the streets are wet.
 -/
-def p6  : PLExpr := sorry
+def p6  : PLExpr := itsRaining ⇒ streetWet
 
 /-!
 Whenever the sprinkler's running the streets are wet.
 -/
-def p7  : PLExpr := sorry
+def p7  : PLExpr := sprinklerOn ⇒ streetWet
 
 /-!
 If (a) it's raining or the sprinkler's running, then (b) if
@@ -192,41 +183,36 @@ _________. What is the conclusion? Write the expression in PL.
 -/
 
 def p8  : PLExpr := (itsRaining ∨ sprinklerOn) ⇒ (itsRaining ⇒ streetWet) ⇒ (sprinklerOn ⇒ streetWet) ⇒ streetWet
-#eval! is_valid p8
-#eval! is_sat p8
-#eval! is_unsat p8
-
-
 
 /-!
 If whenever it's raining, the streets are wet, then whenever the
 streets are wet it's raining.
 -/
-def p9  : PLExpr := sorry
+def p9  : PLExpr := (itsRaining ⇒ streetWet) ⇒ (streetWet ⇒ itsRaining)
 
 
 /-!
 If whever it's raining then bottom, then it's not raining.
 -/
-def p10  : PLExpr := sorry
+def p10  : PLExpr := (itsRaining ⇒ ⊥) ⇒ ¬itsRaining
 
 /-!
 If it's raining or the sprinkler's running then if it's
 not raining then the sprinkler's running.
 -/
-def p11 : PLExpr := sorry
+def p11 : PLExpr := (itsRaining ∨ sprinklerOn) ⇒ (¬itsRaining ⇒ sprinklerOn)
 
 /-!
 If whenever it's raining the streets are wet, then whenever the
 streets are not wet, it must not be raining.
 -/
-def p12 : PLExpr := sorry
+def p12 : PLExpr := (itsRaining ⇒ streetWet) ⇒ (¬streetWet ⇒ ¬itsRaining)
 
 /-!
-QUIZ: challenges to come
--/
+
 def p13 : PLExpr := sorry
 def p14 : PLExpr := sorry
 def p15 : PLExpr := sorry
+-/
 
 end cs2120f24
